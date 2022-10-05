f535821356b92efb977807f96b7eea0be3246d72
Md Shahed Hussain <hussain@radiologics.com>
2022-10-05 17:53:04 +0000

    Rest test created for XXX-190 in TestUsersApi, to test user xml

--- src/test/java/org/nrg/testing/xnat/tests/TestUsersApi.java
+++ src/test/java/org/nrg/testing/xnat/tests/TestUsersApi.java
@@ -1,15 +1,14 @@
 package org.nrg.testing.xnat.tests;
-
+import org.nrg.testing.annotations.AddedIn;
 import org.nrg.testing.annotations.TestRequires;
 import org.nrg.testing.xnat.BaseXnatRestTest;
 import org.nrg.xnat.interfaces.XnatInterface;
 import org.nrg.xnat.pogo.users.User;
 import org.nrg.xnat.rest.PermissionsException;
+import org.nrg.xnat.versions.Xnat_1_8_6;
 import org.testng.annotations.Test;
-
 import java.util.List;
 import java.util.Optional;
-
 import static org.nrg.testing.TestGroups.USERS;
 import static org.testng.AssertJUnit.*;
 
@@ -32,6 +31,15 @@ public class TestUsersApi extends BaseXnatRestTest {
             fail("Attempt to read site user list as nonadmin should have failed with strict security on.");
         } catch (PermissionsException ignored) {}
         verifyUserPresent(mainAdminInterface().readSiteUsers(), genericUser.getUsername());
+    }
+    @Test
+    @AddedIn(Xnat_1_8_6.class)
+    @TestRequires(users = 1, admin = true)
+    public void testUserXml() {
+        final User genericUser = getGenericUser();
+        String userXml = mainQueryBase().get(formatXnatUrl("/app/action/XDATActionRouter/xdataction/xml/search_element/xdat:user/search_field/xdat:user.login/search_value/", genericUser.getUsername(), "/popup/true")).getBody().prettyPrint();
+        assertTrue(userXml.contains("xdat:firstname"));
+        assertFalse(userXml.contains("xdat:primary_password"));
     }
 
     private void verifyUserPresent(List<User> users, String expectedUsername) {
