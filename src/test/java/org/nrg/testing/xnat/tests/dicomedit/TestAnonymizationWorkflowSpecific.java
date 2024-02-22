e32e42da5c61b86a33610392d18c5950b3e78bc9
Charlie Moore <charlesmoore@flywheel.io>
2024-02-22 11:20:30 -0600

    XNAT-8018 adds some tests to make sure project anon does not run in postarchive scenarios when toggled off

--- src/test/java/org/nrg/testing/xnat/tests/dicomedit/TestAnonymizationWorkflowSpecific.java
+++ src/test/java/org/nrg/testing/xnat/tests/dicomedit/TestAnonymizationWorkflowSpecific.java
@@ -1,5 +1,7 @@
 package org.nrg.testing.xnat.tests.dicomedit;
 
+import org.nrg.testing.CommonStringUtils;
+import org.nrg.testing.annotations.AddedIn;
 import org.nrg.testing.annotations.Basic;
 import org.nrg.testing.annotations.TestRequires;
 import org.nrg.testing.dicom.ProjectScript;
@@ -9,11 +11,16 @@ import org.nrg.testing.dicom.XnatVariablesScript;
 import org.nrg.testing.enums.TestData;
 import org.nrg.testing.xnat.XnatObjectUtils;
 import org.nrg.xnat.pogo.AnonScript;
+import org.nrg.xnat.pogo.Project;
+import org.nrg.xnat.pogo.Share;
+import org.nrg.xnat.pogo.Subject;
 import org.nrg.xnat.pogo.experiments.ImagingSession;
+import org.nrg.xnat.versions.Xnat_1_8_10;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 import static org.nrg.testing.TestGroups.ANONYMIZATION;
 import static org.nrg.testing.TestGroups.SMOKE;
@@ -33,41 +40,61 @@ public class TestAnonymizationWorkflowSpecific extends BaseAnonymizationTest {
     @Test(groups = {SMOKE, ANONYMIZATION})
     @Basic
     public void testSubjectRelabel_4P_4S() {
-        performSubjectRelabelAnonTest(projectAnonDE4, siteAnonDE4);
+        new SubjectRelabelAnonTest(projectAnonDE4, siteAnonDE4).run();
     }
 
     public void testSubjectRelabel_4P_6S() {
-        performSubjectRelabelAnonTest(projectAnonDE4, siteAnonDE6);
+        new SubjectRelabelAnonTest(projectAnonDE4, siteAnonDE6).run();
     }
 
     public void testSubjectRelabel_6P_4S() {
-        performSubjectRelabelAnonTest(projectAnonDE6, siteAnonDE4);
+        new SubjectRelabelAnonTest(projectAnonDE6, siteAnonDE4).run();
     }
 
     @Test(groups = {SMOKE, ANONYMIZATION})
     @Basic
     public void testSubjectRelabel_6P_6S() {
-        performSubjectRelabelAnonTest(projectAnonDE6, siteAnonDE6);
+        new SubjectRelabelAnonTest(projectAnonDE6, siteAnonDE6).run();
+    }
+
+    @AddedIn(Xnat_1_8_10.class)
+    public void testSubjectRelabelAnonOff() {
+        new SubjectRelabelAnonTest(projectAnonDE6, siteAnonDE6, false).run();
     }
 
     @Test(groups = {SMOKE, ANONYMIZATION})
     @Basic
     public void testSessionRelabel_4P_4S() {
-        performSessionRelabelAnonTest(projectAnonDE4, siteAnonDE4);
+        new SessionRelabelAnonTest(projectAnonDE4, siteAnonDE4).run();
     }
 
     public void testSessionRelabel_4P_6S() {
-        performSessionRelabelAnonTest(projectAnonDE4, siteAnonDE6);
+        new SessionRelabelAnonTest(projectAnonDE4, siteAnonDE6).run();
     }
 
     public void testSessionRelabel_6P_4S() {
-        performSessionRelabelAnonTest(projectAnonDE6, siteAnonDE4);
+        new SessionRelabelAnonTest(projectAnonDE6, siteAnonDE4).run();
     }
 
     @Test(groups = {SMOKE, ANONYMIZATION})
     @Basic
     public void testSessionRelabel_6P_6S() {
-        performSessionRelabelAnonTest(projectAnonDE6, siteAnonDE6);
+        new SessionRelabelAnonTest(projectAnonDE6, siteAnonDE6).run();
+    }
+
+    @AddedIn(Xnat_1_8_10.class)
+    public void testSessionRelabelAnonOff() {
+        new SessionRelabelAnonTest(projectAnonDE6, siteAnonDE6, false).run();
+    }
+
+    @AddedIn(Xnat_1_8_10.class)
+    public void testSessionReassignAnonOff() {
+        new SessionReassignmentAnonTest(projectAnonDE6, siteAnonDE6, false).run();
+    }
+
+    @AddedIn(Xnat_1_8_10.class)
+    public void testSessionMoveProjectAnonOff() {
+        new SessionMoveProjectAnonTest(projectAnonDE6, siteAnonDE6, false).run();
     }
 
     public void testXnatVariablesDE4() {
@@ -78,54 +105,104 @@ public class TestAnonymizationWorkflowSpecific extends BaseAnonymizationTest {
         new VariableScriptTest().run();
     }
 
-    private void performSubjectRelabelAnonTest(AnonScript projectScript, AnonScript siteScript) {
-        final ImagingSession session = new GenericAnonymizationTest<>()
-                .withSetup(setupUpDisabledScripts(projectScript, siteScript))
-                .withDisabledScripts(Arrays.asList(COMMON_PROJECT_VALIDATION, COMMON_SITE_VALIDATION))
-                .run();
+    private class SubjectRelabelAnonTest extends PostArchiveAnonTest {
+        private SubjectRelabelAnonTest(AnonScript projectScript, AnonScript siteScript, boolean enablePostArchiveAnon) {
+            super(projectScript, siteScript, enablePostArchiveAnon);
+        }
 
-        new GenericAnonymizationTest<>()
-                .withSetup(() -> {
-                    mainAdminInterface().enablePostArchiveAnon();
-                    mainInterface().waitForAutoRun(session);
-                    mainInterface().enableProjectAnonScript(anonProject);
-                    mainInterface().relabelSubject(session.getSubject(), "NEWLABEL");
-                }).withUpload(() -> session)
-                .withEnabledScripts(Collections.singletonList(COMMON_PROJECT_VALIDATION))
-                .withDisabledScripts(Collections.singletonList(COMMON_SITE_VALIDATION))
-                .run();
+        private SubjectRelabelAnonTest(AnonScript projectScript, AnonScript siteScript) {
+            this(projectScript, siteScript, true);
+        }
+
+        @Override
+        protected void postArchiveAction(ImagingSession session) {
+            mainInterface().relabelSubject(session.getSubject(), "NEWLABEL");
+        }
     }
 
-    private void performSessionRelabelAnonTest(AnonScript projectScript, AnonScript siteScript) {
-        mainAdminInterface().setSiteAnonScript(siteScript);
-        mainInterface().setProjectAnonScript(anonProject, projectScript);
-        mainInterface().disableProjectAnonScript(anonProject);
-        mainAdminInterface().disableSiteAnonScript();
+    private class SessionRelabelAnonTest extends PostArchiveAnonTest {
+        private SessionRelabelAnonTest(AnonScript projectScript, AnonScript siteScript, boolean enablePostArchiveAnon) {
+            super(projectScript, siteScript, enablePostArchiveAnon);
+        }
 
-        final ImagingSession session = new GenericAnonymizationTest<>()
-                .withSetup(setupUpDisabledScripts(projectScript, siteScript))
-                .withDisabledScripts(Arrays.asList(COMMON_PROJECT_VALIDATION, COMMON_SITE_VALIDATION))
-                .run();
+        private SessionRelabelAnonTest(AnonScript projectScript, AnonScript siteScript) {
+            this(projectScript, siteScript, true);
+        }
 
-        new GenericAnonymizationTest<>()
-                .withSetup(() -> {
-                    mainAdminInterface().enablePostArchiveAnon();
-                    mainInterface().waitForAutoRun(session);
-                    mainInterface().enableProjectAnonScript(anonProject);
-                    mainInterface().relabelSubjectAssessor(session, "NEWLABEL");
-                }).withUpload(() -> session)
-                .withEnabledScripts(Collections.singletonList(COMMON_PROJECT_VALIDATION))
-                .withDisabledScripts(Collections.singletonList(COMMON_SITE_VALIDATION))
-                .run();
+        @Override
+        protected void postArchiveAction(ImagingSession session) {
+            mainInterface().relabelSubjectAssessor(session, "NEWLABEL");
+        }
     }
 
-    private Runnable setupUpDisabledScripts(AnonScript projectScript, AnonScript siteScript) {
-        return () -> {
-            mainAdminInterface().setSiteAnonScript(siteScript);
-            mainInterface().setProjectAnonScript(anonProject, projectScript);
-            mainInterface().disableProjectAnonScript(anonProject);
-            mainAdminInterface().disableSiteAnonScript();
-        };
+    private class SessionReassignmentAnonTest extends PostArchiveAnonTest {
+        private SessionReassignmentAnonTest(AnonScript projectScript, AnonScript siteScript, boolean enablePostArchiveAnon) {
+            super(projectScript, siteScript, enablePostArchiveAnon);
+        }
+
+        @Override
+        protected void postArchiveAction(ImagingSession session) {
+            final Subject destinationSubject = new Subject(anonProject);
+            mainInterface().createSubject(destinationSubject);
+            mainInterface().moveSubjectAssessorToOtherSubject(session, destinationSubject);
+        }
+    }
+
+    private class SessionMoveProjectAnonTest extends PostArchiveAnonTest {
+        private SessionMoveProjectAnonTest(AnonScript projectScript, AnonScript siteScript, boolean enablePostArchiveAnon) {
+            super(projectScript, siteScript, enablePostArchiveAnon);
+        }
+
+        @Override
+        protected void postArchiveAction(ImagingSession session) {
+            final Project otherProject = registerTempProject();
+            mainInterface().createProject(otherProject);
+            mainQueryBase()
+                    .queryParam("primary", true)
+                    .put(CommonStringUtils.formatUrl(mainInterface().subjectAssessorUrl(session), "projects", otherProject.getId()))
+                    .then()
+                    .assertThat()
+                    .statusCode(200);
+            mainInterface().shareSubject(session.getPrimaryProject(), session.getSubject(), new Share(otherProject));
+            session.setPrimaryProject(otherProject);
+            anonProject = otherProject;
+        }
+    }
+
+    private abstract class PostArchiveAnonTest extends GenericAnonymizationTest<PostArchiveAnonTest> {
+
+        private ImagingSession session;
+
+        private PostArchiveAnonTest(AnonScript projectScript, AnonScript siteScript, boolean enablePostArchiveAnon) {
+            withSetup(() -> {
+                session = new GenericAnonymizationTest<>()
+                        .withSetup(() -> {
+                            mainAdminInterface().setSiteAnonScript(siteScript);
+                            mainInterface().setProjectAnonScript(anonProject, projectScript);
+                            mainInterface().disableProjectAnonScript(anonProject);
+                            mainAdminInterface().disableSiteAnonScript();
+                        })
+                        .withDisabledScripts(Arrays.asList(COMMON_PROJECT_VALIDATION, COMMON_SITE_VALIDATION))
+                        .run();
+                mainAdminInterface().setPostArchiveAnonStatus(enablePostArchiveAnon);
+                mainInterface().waitForAutoRun(session);
+                mainInterface().enableProjectAnonScript(anonProject);
+                postArchiveAction(session);
+            });
+
+            final List<ScriptValidation> enabledScripts = new ArrayList<>();
+            final List<ScriptValidation> disabledScripts = new ArrayList<>();
+            (enablePostArchiveAnon ? enabledScripts : disabledScripts).add(COMMON_PROJECT_VALIDATION);
+            disabledScripts.add(COMMON_SITE_VALIDATION);
+
+            withEnabledScripts(enabledScripts);
+            withDisabledScripts(disabledScripts);
+
+            withUpload(() -> session);
+        }
+
+        protected abstract void postArchiveAction(ImagingSession session);
+
     }
 
     private class VariableScriptTest extends BasicAnonymizationTest {
